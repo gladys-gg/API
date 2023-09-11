ba63ec6c18b2fbd8d5deeb094fc4d255c079613a
gladys-gg <gladysmwangi04@yahoo.com>
2023-09-11 23:38:22 +0300

    created the api project

--- Endpoints/Endpoints/settings.py
+++ Endpoints/Endpoints/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #own
+    'apiEndpoints',
+    'rest-framework',
+
 ]
 
 MIDDLEWARE = [
